a9d8724f9a6e8ed17e829492a51f58cb5741e40d
Restitutor-Orbis <65425000+Restitutor-Orbis@users.noreply.github.com>
2021-11-22 14:51:51 +0100

    Implement/entity framework (#4)
    * Setup EF classes
    
    Co-authored-by: Crushaderdk <Crushaderdk@users.noreply.github.com>
    
    * Forgot this one
    
    * Implement EF further
    
    Co-authored-by: Crushaderdk <antbr@itu.dk>
    
    Co-authored-by: Crushaderdk <Crushaderdk@users.noreply.github.com>
    Co-authored-by: Crushaderdk <antbr@itu.dk>

---
+++ Infrastructure/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+global using System.ComponentModel.DataAnnotations;
+
+global using ProjectBank.Core;
+global using static ProjectBank.Core.ProjectStatus;
