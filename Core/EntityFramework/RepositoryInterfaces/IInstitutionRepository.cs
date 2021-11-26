5518b87ff59cf2b5f596586ae322cbb3c62c19a5
Restitutor-Orbis <restitutororbis@tutanota.com>
2021-11-26 23:10:05 +0100

    Update interfaces

--- Core/EntityFramework/RepositoryInterfaces/IInstitutionRepository.cs
+++ Core/EntityFramework/RepositoryInterfaces/IInstitutionRepository.cs
@@ -3,6 +3,6 @@ namespace ProjectBank.Core.EF.Repository;
 public interface IInstitutionRepository 
 {
     Task<(Response, InstitutionDTO)> CreateAsync(InstitutionCreateDTO institution);
-    Task<InstitutionDTO> ReadFacultyByIDAsync(int insitutionID);
+    Task<InstitutionDTO> ReadByIDAsync(int insitutionID);
     Task<IReadOnlyCollection<InstitutionDTO>> ReadAllAsync();
 }
--- Core/EntityFramework/RepositoryInterfaces/IUserRepository.cs
+++ Core/EntityFramework/RepositoryInterfaces/IUserRepository.cs
@@ -2,7 +2,10 @@ namespace ProjectBank.Core.EF.Repository;
 
 public interface IUserRepository 
 {
-    Task<(Response, UserDTO)> CreateAsync(UserCreateDTO user);
+    Task<(Response, StudentDTO)> CreateAsync(StudentCreateDTO user);
+    Task<(Response, SupervisorDTO)> CreateAsync(SupervisorCreateDTO user);
     Task<UserDTO> ReadByID(int userID);
-    Task<IReadOnlyCollection<UserID>> ReadAllAsync();
+    Task<IReadOnlyCollection<UserDTO>> ReadAllAsync();
+    Task<IReadOnlyCollection<StudentDTO>> ReadAllStudentsAsync();
+    Task<IReadOnlyCollection<SupervisorDTO>> ReadAllSupervisorsAsync();
 }
