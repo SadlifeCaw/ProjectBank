826e7326f8f2a5a6b4b8790ca8cba994ad2c7fda
SadlifeCaw <caws@itu.dk>
2021-11-26 16:04:12 +0100

    Added TagGenerator Algorithm and Tests

---
+++ Infrastructure/TagGenerator/Program.cs
@@ -0,0 +1,24 @@
+﻿using Generator;
+
+class Program {
+    public static void Main(string[] args)
+    {
+
+        ITagGenerator trie = new TagGenerator();
+
+        trie.Add("kom");
+        trie.Add("ko");
+        trie.Add("læge");
+
+        trie.Build();
+      
+        String s = "Hej Jeg er blevet sparket af en ko, og ko jeg Ko KO har brug for en læge";
+
+        s = s.ToLower(); 
+        
+        foreach (string word in trie.Find(s))
+        {
+            Console.WriteLine(word);
+        }
+    }
+}
