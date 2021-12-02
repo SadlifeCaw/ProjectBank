3f2da69effd5f74be987c0da617bc994f4adc521
Restitutor-Orbis <restitutororbis@tutanota.com>
2021-12-02 17:55:08 +0100

    Fix Foreign Key constraint when updating DB

--- Infrastructure/User/Supervisor.cs
+++ Infrastructure/User/Supervisor.cs
@@ -4,5 +4,13 @@ public class Supervisor : User
 {
     [Required]
     public Faculty Faculty {get;set;}
+
+    public Supervisor(string Email, Institution Institution, string FirstName, string LastName, ICollection<Project> Projects, Faculty Faculty)
+    : base(Email, Institution, FirstName, LastName, Projects)
+    {
+        this.Faculty = Faculty;
+    }
+
+    public Supervisor() {}
 }
 
