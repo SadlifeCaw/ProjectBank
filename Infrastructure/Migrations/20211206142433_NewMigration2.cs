61c7a0e447ace2af112ecb0a7bbec1f4cde6c68a
SadlifeCaw <caws@itu.dk>
2021-12-06 15:39:48 +0100

    Small updates

---
+++ Infrastructure/Migrations/20211206142433_NewMigration2.cs
@@ -0,0 +1,156 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class NewMigration2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Users_Courses_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_CourseId",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "CourseId",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "SignatureId",
+                table: "Projects",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateTable(
+                name: "CourseStudent",
+                columns: table => new
+                {
+                    CoursesId = table.Column<int>(type: "int", nullable: false),
+                    StudentsId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseStudent", x => new { x.CoursesId, x.StudentsId });
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Courses_CoursesId",
+                        column: x => x.CoursesId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_CourseStudent_Users_StudentsId",
+                        column: x => x.StudentsId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Signatures",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1")
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Signatures", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_CategoryId",
+                table: "Projects",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Projects_SignatureId",
+                table: "Projects",
+                column: "SignatureId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CourseStudent_StudentsId",
+                table: "CourseStudent",
+                column: "StudentsId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Categories_CategoryId",
+                table: "Projects",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Projects_Signatures_SignatureId",
+                table: "Projects",
+                column: "SignatureId",
+                principalTable: "Signatures",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Categories_CategoryId",
+                table: "Projects");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Projects_Signatures_SignatureId",
+                table: "Projects");
+
+            migrationBuilder.DropTable(
+                name: "CourseStudent");
+
+            migrationBuilder.DropTable(
+                name: "Signatures");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_CategoryId",
+                table: "Projects");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Projects_SignatureId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Projects");
+
+            migrationBuilder.DropColumn(
+                name: "SignatureId",
+                table: "Projects");
+
+            migrationBuilder.AddColumn<int>(
+                name: "CourseId",
+                table: "Users",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_CourseId",
+                table: "Users",
+                column: "CourseId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Users_Courses_CourseId",
+                table: "Users",
+                column: "CourseId",
+                principalTable: "Courses",
+                principalColumn: "Id");
+        }
+    }
+}
