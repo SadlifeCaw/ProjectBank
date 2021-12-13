1ecb1588cbfb9b0c34fc7b5dc8932552a6da09db
Restitutor-Orbis <65425000+Restitutor-Orbis@users.noreply.github.com>
2021-12-13 18:04:18 +0100

    Post Project (#49)
    * Add Post Project (only with user generated tag)
    
    
    Co-authored-by: SadlifeCaw <74045978+SadlifeCaw@users.noreply.github.com>

--- Server/Controllers/TagsController.cs
+++ Server/Controllers/TagsController.cs
@@ -19,12 +19,12 @@ namespace ProjectBank.Server.Controllers;
 public class TagController : ControllerBase
 {
     private readonly ILogger<TagController> _logger;
-    private readonly ITagRepository _TagRepository;
+    private readonly ITagRepository _repository;
     
     public TagController(ILogger<TagController> logger, ITagRepository TagRepository)
     {
         _logger = logger;
-        _TagRepository = TagRepository;
+        _repository = TagRepository;
     }
 
     [AllowAnonymous]
@@ -32,6 +32,20 @@ public class TagController : ControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(TagDTO), StatusCodes.Status200OK)]
     public async Task<ActionResult<TagDTO>> GetTag(int TagId) 
-    =>(await _TagRepository.ReadTagByIDAsync(TagId)).ToActionResult();
-}
+    =>(await _repository.ReadTagByIDAsync(TagId)).ToActionResult();
 
+    [AllowAnonymous]
+    [HttpGet]
+    public async Task<IReadOnlyCollection<TagDTO>> Get()
+        => await _repository.ReadAllAsync();
+
+
+    [AllowAnonymous]
+    [ProducesResponseType(typeof(TagDTO), 201)]
+    [HttpPost]
+    public async Task<IActionResult> Post(TagCreateDTO tag)
+    {
+        var created = (await _repository.CreateAsync(tag)).Item2;
+        return CreatedAtAction(nameof(Get), new { created.Id }, created);
+    }
+}
